a4c18ba6d476eae83a4369a508bbb32a37ef3fab
SalyOne <salome.batkhadze@gmail.com>
2024-04-14 02:23:11 +0400

    feat: add blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import {MainSliderComponent} from "./components/main-slider/main-slider.componen
 import {FactsSliderComponent} from "./components/facts-slider/facts-slider.component";
 import {NewsSidebarComponent} from "./components/news-sidebar/news-sidebar.component";
 import {PopularSidebarComponent} from "./components/popular-sidebar/popular-sidebar.component";
+import {BlogComponent} from "./components/blog/blog.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainSliderComponent, FactsSliderComponent, NewsSidebarComponent, PopularSidebarComponent],
+  imports: [RouterOutlet, HeaderComponent, MainSliderComponent, FactsSliderComponent, NewsSidebarComponent, PopularSidebarComponent, BlogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
