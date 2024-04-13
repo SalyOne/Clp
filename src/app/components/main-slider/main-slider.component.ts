22739c7b83dd6d2fec16a71ca919c81a746d7b51
SalyOne <salome.batkhadze@gmail.com>
2024-04-13 23:16:48 +0400

    feat: add slider item , init upload

---
+++ src/app/components/main-slider/main-slider.component.ts
@@ -0,0 +1,13 @@
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+
+@Component({
+  selector: 'app-main-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-slider.component.html',
+  styleUrl: './main-slider.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class MainSliderComponent {
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        xs: '328px',
+        md: '928px',
+        lg: '1128px',
+        xl: '1321px',
+
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary-1': '#585EE3',
+      'primary-2': '#F5F8FF',
+      'black': '#000000',
+      'midnight': '#1D2D35',
+      'gray': '#657178',
+      'light-gray': '#EFF4F5',
+      'white': '#ffffff',
+    },
+    boxShadow: {
+      'light': '0px 0px 40px 0px rgba(0,0,0,0.08);',
+      'dark': '0px 0px 20px 0px rgba(0,0,0,0.1);',
+    },
+
+    extend: {
+      fontFamily:{
+        "noto":['"Noto Sans Georgian"', "sans-serif"],
+        "caps":['"BPG Banner Caps"', "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
